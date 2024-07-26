20c98514e66965a19cb308f37fff24a35deafee3
Prasanna Kumar Pandey <prasanna.pandey99@gmail.com>
2024-07-26 22:58:24 +0530

    I have uploaded my assignment

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def home():
-    return'<h1>Hello! Welcome to my Web Page of G23AI2061'
+    return'Hello! Welcome to my Web Page of G23AI2061'
 
-app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port =5000)
